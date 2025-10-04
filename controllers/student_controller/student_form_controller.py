27ffd1fa5622e36214d9cce8466e73e7a6038c26
fatemeh <fatemehrouhafzaei.86@gmail.com>
2025-10-04 20:50:21 +0330

    Add grade level and field of study columns to student table

--- controllers/student_controller/student_form_controller.py
+++ controllers/student_controller/student_form_controller.py
@@ -2,7 +2,7 @@ from messages.errors import ErrorMessages
 from tkinter import messagebox
 
 class StudentFormController:
-    def __init__(self, model , view):
+    def init(self, model, view):
         self.model = model
         self.view = view
         self.view.save_button.config(command=self.save_form)
@@ -10,11 +10,10 @@ class StudentFormController:
     def save_form(self):
         try:
             data = self.view.get_student_form()
-            self.model.add_student(data["name"], data["national_code"], data["mobile"], data["password"])
+            self.model.add_student(data["name"],data["national_code"],data["mobile"],data["password"],data["grade_level"],data["field_of_study"])
             messagebox.showinfo("موفقیت", "دانش‌آموز با موفقیت ثبت شد.")
         except Exception as e:
             messagebox.showerror("خطا", f"ثبت‌نام انجام نشد: {e}")
-
 
     def get_students(self):
         return self.model.get_all_students()
--- models/student_model.py
+++ models/student_model.py
@@ -1,13 +1,13 @@
 class StudentModel:
-    def __init__(self, db):
+    def init(self, db):
         self.db = db
 
-    def add_student(self, name , national_code , mobile , password):
-        self.db.execute("INSERT INTO students (name, national_code , mobile , password) VALUES (?,?,?,?)"
-                        , (name, national_code , mobile , password))
+    def add_student(self, name, national_code, mobile, password, grade_level, field_of_study):
+        self.db.execute("INSERT INTO students (name, national_code, mobile, password, grade_level, field_of_study) VALUES (?,?,?,?,?,?)",
+                        (name, national_code, mobile, password, grade_level, field_of_study))
 
     def get_all_students(self):
-        return self.db.fetchall("SELECT id, name, national_code , mobile FROM students")
+        return self.db.fetchall("SELECT id, name, national_code, mobile, grade_level, field_of_study FROM students")
 
     def delete_student(self, student_id):
         self.db.execute("DELETE FROM students WHERE id = ?", (student_id,))
